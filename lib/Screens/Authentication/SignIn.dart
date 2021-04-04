fd7256ebfaeba52ea68df4c1c56bbab9f54041ae
Wiil Craven <topoika>
2021-04-04 21:43:36 +0300

    Last Commit for today

---
+++ lib/Screens/Authentication/SignIn.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SignIn'),
+      ),
+      body: Container(),
+    );
+  }
+}
