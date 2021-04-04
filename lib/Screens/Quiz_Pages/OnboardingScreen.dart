a3e75e88c40fc91203e99b99166a23a99ca835dd
Wiil Craven <topoika>
2021-04-04 21:43:36 +0300

    Last Commit for today

---
+++ lib/Screens/Quiz_Pages/OnboardingScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  Onboarding({Key key}) : super(key: key);
+
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Onboarding'),
+      ),
+      body: Container(),
+    );
+  }
+}
